82bc576f1ee6f0db5632c538cbf706c7b12f61be
aliasav <saurabhav.torres@gmail.com>
2016-08-19 17:46:46 +0530

    added comments

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import urllib
 import os
 from bs4 import BeautifulSoup
 
+
 class PlayStore():
+	""" Play Store class includes all methods required for scraping a valid url
+		Initialized with a valid package name
+	"""
 
 	def __init__(self, package_name):		
+		""" init fails on invalid package name """	
 		
 		self._cover_image = None
 		self._description = None
@@ -26,14 +31,19 @@ class PlayStore():
 
 
 	def _validate_play_store_package_name(self, p):
+		""" validate package name string """
 		regex =  "([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$"
 		return re.match(regex, p)
 
 	def _generate_play_store_url(self):
+		""" generate google play store link for a valid package name """
+
 		# https://play.google.com/store/apps/details?id=com.icicibank.pockets
 		return ("https://play.google.com/store/apps/details?id=" + str(self.__package_name))
 
 	def _fetch_url_content(self):
+		""" fetch html content of play store link """
+
 		url = self._generate_play_store_url()
 		l = urllib.urlopen(url)
 		if l.getcode() == 200:
@@ -44,6 +54,8 @@ class PlayStore():
 			sys.exit(1)
 
 	def _generate_soup(self):
+		""" generates bs4 soup object """
+
 		try:
 			content = self._fetch_url_content()
 			soup = BeautifulSoup(content, "html.parser")
@@ -55,6 +67,8 @@ class PlayStore():
 			return soup
 
 	def _parse_soup(self):
+		""" parse soup to fetch all required content and save on disk """
+
 		soup = self._generate_soup()
 		cover_image = self._fetch_cover_image(soup)
 		description = self._fetch_description(soup)
@@ -74,6 +88,8 @@ class PlayStore():
 		# print("\nScreenshots: %s" %screenshots)
 
 	def _fetch_cover_image(self, soup):
+		""" fetches cover image url """
+
 		cover_container = soup.find_all('div', {"class": "cover-container"})
 		for c in cover_container:
 			cover_image = c.find_all('img', {"class": "cover-image"})			
@@ -84,6 +100,8 @@ class PlayStore():
 			return None
 
 	def _fetch_description(self, soup):
+		""" fetches description text """
+
 		content_container = soup.find_all('div', {"class": "show-more-content text-body"})[0]
 		description_container = content_container.find_all("div", {"jsname": "C4s9Ed"})
 		if len(description_container)>0:
@@ -93,6 +111,8 @@ class PlayStore():
 			return None
 		
 	def _fetch_screenshots(self, soup):
+		""" fetches screenshots urls """
+
 		img_containers = soup.find_all("img", {"class": "full-screenshot"})
 		screenshots = []
 		for i in img_containers:
@@ -101,6 +121,9 @@ class PlayStore():
 		return screenshots
 
 	def _setup_folder(self):
+		""" sets up a folder with package name as folder name and deletes
+		if already existing  """
+
 		if not os.path.exists(self.__package_name):
 			os.makedirs(self.__package_name)
 		else:
@@ -112,25 +135,28 @@ class PlayStore():
 			
 
 	def _download_and_save(self, url, fname):
+		""" download and save image file with .png extension """
 		urllib.urlretrieve(url, self.__package_name+"/"+fname+".png")
 
 	def _save_cover_image(self):
+		""" save cover image to disk """
 		self._download_and_save(self._cover_image, "cover_image")
 
 	def _save_screenshots(self):
+		""" save screenshots to disk """
 		i = 0
 		for s in self._screenshots:			
 			self._download_and_save(s, "s"+str(i))
 			i += 1
 
 	def _save_description(self):
+		""" save description to a file on disk """
 		f = open(self.__package_name+"/"+"desc", 'wb')
 		f.write(self._description.encode("utf8"))
 		f.close()
 
 if __name__== "__main__":
 	package_name = str(raw_input("Enter a package name: "))
-	#package_name = "com.flipkart.android"
 	play_store = PlayStore(package_name)	
 	play_store._parse_soup()
 
